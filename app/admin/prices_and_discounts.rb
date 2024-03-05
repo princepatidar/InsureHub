9e930d288ca830a5b274f709df8e8fb2aea28cd2
princepatidar <princepatidar342@gmail.com>
2024-03-05 17:56:28 +0530

    Add admin abiity to update item prices and discounts

---
+++ app/admin/prices_and_discounts.rb
@@ -0,0 +1,50 @@
+ActiveAdmin.register ItemWarranty, as: 'PricesAndDiscounts' do
+  menu label: 'Prices And Discounts'
+  permit_params :warranty_price_percent, :discount_percent, :discount_start_date, :discount_end_date
+
+  index do
+    column :item
+    column :warranty
+    column :warranty_price_percent
+    column :discount_percent
+    actions
+  end
+
+  show title: 'Price & discount' do
+    attributes_table do
+      row :item
+      row :warranty
+      row :warranty_price_percent
+      row :discount_percent
+      row :discount_start_date
+      row :discount_end_date
+    end
+  end
+
+  form title: 'Price & discount' do |f|
+    f.inputs do
+      f.input :item, input_html: { disabled: true }
+      f.input :warranty, input_html: { disabled: true }
+      f.input :warranty_price_percent
+      f.input :discount_percent
+      f.input :discount_start_date
+      f.input :discount_end_date
+    end
+    f.actions
+  end
+
+  controller do
+    def scoped_collection
+      super.includes(:item, :warranty)
+    end
+  end
+
+  action_item 'Back', only: :show do
+    link_to('Back', admin_prices_and_discounts_path)
+  end
+
+  filter :warranty
+  filter :item
+
+  actions :show, :edit, :update, :index
+end
--- db/migrate/20240302121223_create_item_warranties.rb
+++ db/migrate/20240302121223_create_item_warranties.rb
@@ -2,10 +2,9 @@ class CreateItemWarranties < ActiveRecord::Migration[7.0]
   def change
     create_table :item_warranties do |t|
       t.float :warranty_price_percent, default: 0.0
-      t.boolean :active, default: true
       t.float :discount_percent, default: 0.0
-      t.datetime :discount_start_date
-      t.datetime :discount_end_date
+      t.date :discount_start_date
+      t.date :discount_end_date
       t.references :item, null: false, index: true
       t.references :warranty, null: false, index: true
 
